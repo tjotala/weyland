f7d96a39e393f271277aa3bcb88794897ebafb2a
Tapani Otala <github@tjotala.com>
2017-05-31 11:21:03 -0700

    Tweak the converter class a bit.

--- app/lib/converter.rb
+++ app/lib/converter.rb
@@ -2,11 +2,11 @@ require File.join(Platform::PLATFORM_PATH, 'converter.rb')
 
 class Converter
 	def version
-		Platform::run("#{TOOL_PATH} --version")
+		Platform::run("#{TOOL_PATH} --version").chomp.strip
 	end
 
 	def convert(src, dest)
 		cmd = "#{TOOL_PATH} --without-gui --export-plain-svg=#{dest} --export-text-to-path #{src}"
-		Platform::run("echo #{cmd}; #{cmd}")
+		version + "\n" + cmd + "\n" + Platform::run(cmd)
 	end
 end
